a7b8dd2ff7f375d5581dac9ec2ed07d7fa676bb2
Prabhu Suriya Kumar <prabhu_suriyakumar@trimble.com>
2020-08-20 15:10:51 +0530

    win check; message display

--- game.js
+++ game.js
@@ -1,16 +1,12 @@
 const o = "o";
 const x = "x";
-const players = ["A", "B"];
-const playersSymbols = {
-  A: o,
-  B: x,
+const players = {
+  A: { name: "Mario", symbol: o },
+  B: { name: "Luigi", symbol: x },
 };
 let currentPlayer;
-const gameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
+let gameBoard;
+const infoEl = document.getElementById("info");
 
 function setupGame() {
   const boxes = document.querySelectorAll(".box");
@@ -18,17 +14,37 @@ function setupGame() {
 }
 
 function startGame() {
-  currentPlayer = players[0];
+  gameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+  currentPlayer = players.A;
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.classList.remove("symbol-o");
+    box.classList.remove("symbol-x");
+  });
+  showMessage(`New Game. ${currentPlayer.name}'s playing..`);
+}
+function showMessage(msg) {
+  infoEl.innerText = msg;
 }
 
 function handleBoxClick(e) {
   const boxId = e.target.getAttribute("data-box");
-  const symbol = playersSymbols[currentPlayer];
+  const symbol = currentPlayer.symbol;
   const isValid = addSymbol(boxId, symbol);
   if (isValid) {
     updateBoardUI(boxId, symbol);
     switchPlayer();
     const status = getGameStatus();
+    if (status) {
+      showWinner(status);
+      setTimeout(() => {
+        startGame();
+      }, 2000);
+    }
   }
 }
 
@@ -39,21 +55,68 @@ function updateBoardUI(boxId, symbol) {
 
 function switchPlayer() {
   // TODO make this better
-  currentPlayer = currentPlayer == players[0] ? players[1] : players[0];
+  currentPlayer = currentPlayer == players.B ? players.A : players.B;
+  showMessage(`${currentPlayer.name}'s playing..`);
 }
 
 function addSymbol(boxId, symbol) {
   const [row, col] = boxId.split("-");
-  let current = gameBoard[row - 1][col - 1];
-  if (!current) {
-    current = symbol;
+  if (!gameBoard[row - 1][col - 1]) {
+    gameBoard[row - 1][col - 1] = symbol;
     return true;
   }
   return false;
 }
 
 function getGameStatus() {
-  
+  const result = {
+    winner: null,
+  };
+
+  // check rows
+  for (let i = 0; i < 3; i++) {
+    const winningSymbol = checkIfSameSymbols(gameBoard[i]);
+    if (winningSymbol) {
+      result.winner = winningSymbol;
+      return result;
+    }
+  }
+  // check columns
+  for (let i = 0; i < 3; i++) {
+    const winningSymbol = checkIfSameSymbols(gameBoard.map((x) => x[i]));
+    if (winningSymbol) {
+      result.winner = winningSymbol;
+      return result;
+    }
+  }
+  //check diagonal
+  {
+    const boxesToCheck = gameBoard.map((r, i) => r[i]);
+    const winningSymbol = checkIfSameSymbols(boxesToCheck);
+    if (winningSymbol) {
+      result.winner = winningSymbol;
+      return result;
+    }
+  }
+  {
+    const boxesToCheck = gameBoard.map((r, i) => r[2 - i]);
+    const winningSymbol = checkIfSameSymbols(boxesToCheck);
+    if (winningSymbol) {
+      result.winner = winningSymbol;
+      return result;
+    }
+  }
+}
+
+function checkIfSameSymbols([a, b, c]) {
+  return !!a & !!b & !!c & (a == b) && b == c ? a : null;
+}
+
+function showWinner(status) {
+  const w = Object.keys(players).find(
+    (p) => players[p].symbol == status.winner
+  );
+  showMessage(`Winner is ${players[w].name}`);
 }
 
 setupGame();
