cfa8b6bfe7d82cb18e6beb2562baf81f19c9db37
Prabhu Suriya Kumar <prabhu_suriyakumar@trimble.com>
2020-08-20 13:07:06 +0530

    setup player turns; places symbols

--- game.js
+++ game.js
@@ -5,23 +5,46 @@ const playersSymbols = {
   A: o,
   B: x,
 };
+let currentPlayer;
 const gameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-function startGame() {
+function setupGame() {
   const boxes = document.querySelectorAll(".box");
-  boxes.forEach((box) =>
-    box.addEventListener("click", (e) => {console.log('e', e.target.getAttribute('data-box'))})
-  );
+  boxes.forEach((box) => box.addEventListener("click", handleBoxClick));
 }
 
-function updateBoardUI() {}
+function startGame() {
+  currentPlayer = players[0];
+}
 
-function addSymbol(row, col, symbol) {
-  const current = gameBoard[(row, col)];
+function handleBoxClick(e) {
+  const boxId = e.target.getAttribute("data-box");
+  const symbol = playersSymbols[currentPlayer];
+  const isValid = addSymbol(boxId, symbol);
+  if (isValid) {
+    updateBoardUI(boxId, symbol);
+    switchPlayer();
+    const status = getGameStatus();
+  }
+}
+
+function updateBoardUI(boxId, symbol) {
+  const box = document.querySelector(`[data-box='${boxId}']`);
+  box.classList.add(`symbol-${symbol}`);
+}
+
+function switchPlayer() {
+  // TODO make this better
+  currentPlayer = currentPlayer == players[0] ? players[1] : players[0];
+}
+
+function addSymbol(boxId, symbol) {
+  const [row, col] = boxId.split("-");
+  let current = gameBoard[row - 1][col - 1];
   if (!current) {
     current = symbol;
     return true;
@@ -29,4 +52,9 @@ function addSymbol(row, col, symbol) {
   return false;
 }
 
+function getGameStatus() {
+  
+}
+
+setupGame();
 startGame();
